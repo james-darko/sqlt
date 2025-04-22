28f96cbf5d2ae71358e673c312d1f59ed148fdb9
James Darko <james.darko5020@gmail.com>
2025-04-22 18:38:53 -0500

    *

--- transactions.go
+++ transactions.go
@@ -9,26 +9,14 @@ import (
 )
 
 func transaction(ctx context.Context, db *sqlx.DB, imm bool, fn func(conn Tx) error) (rErr error) {
-	driver := db.DriverName()
-	if driver != "libsql" && driver != "sqlite3" {
-		return fmt.Errorf("transactionImm is only supported for libsql and sqlite3 drivers")
-	}
-	conn, err := db.Connx(ctx)
-	if err != nil {
-		return fmt.Errorf("failed to get connection: %w", err)
-	}
-	defer conn.Close()
-	if imm {
-		_, err = conn.ExecContext(ctx, "BEGIN IMMEDIATE")
-	} else {
-		_, err = conn.ExecContext(ctx, "BEGIN")
-	}
+	tx, err := db.BeginTxx(ctx, nil)
 	if err != nil {
 		return fmt.Errorf("failed to start transaction: %w", err)
 	}
+
 	defer func() {
 		if err := recover(); err != nil {
-			if _, rollbackErr := conn.ExecContext(ctx, "ROLLBACK"); rollbackErr != nil {
+			if rollbackErr := tx.Rollback(); rollbackErr != nil {
 				fmt.Printf("failed to rollback transaction in panic: %v", rollbackErr)
 			}
 			var e Error
@@ -39,21 +27,67 @@ func transaction(ctx context.Context, db *sqlx.DB, imm bool, fn func(conn Tx) er
 			}
 		}
 	}()
-	tx := &sqlxTx{
-		ctx:        ctx,
-		conn:       conn,
-		driverName: driver,
+	t := &txWrapper{
+		tx: tx,
 	}
-	err = fn(tx)
+	err = fn(t)
 	if err != nil {
-		if _, rollbackErr := conn.ExecContext(ctx, "ROLLBACK"); rollbackErr != nil {
+		if rollbackErr := tx.Rollback(); rollbackErr != nil {
 			return fmt.Errorf("failed to rollback transaction: %w", rollbackErr)
 		}
 		return err
+	} else {
+		return tx.Commit()
 	}
-	_, err = conn.ExecContext(ctx, "COMMIT")
-	if err != nil {
-		return fmt.Errorf("failed to commit transaction: %w", err)
-	}
-	return nil
 }
+
+// func transaction(ctx context.Context, db *sqlx.DB, imm bool, fn func(conn Tx) error) (rErr error) {
+// 	driver := db.DriverName()
+// 	if driver != "libsql" && driver != "sqlite3" {
+// 		return fmt.Errorf("transactionImm is only supported for libsql and sqlite3 drivers")
+// 	}
+// 	db.Begin()
+// 	conn, err := db.Connx(ctx)
+// 	if err != nil {
+// 		return fmt.Errorf("failed to get connection: %w", err)
+// 	}
+// 	defer conn.Close()
+// 	if imm {
+// 		_, err = conn.ExecContext(ctx, "BEGIN IMMEDIATE")
+// 	} else {
+// 		_, err = conn.ExecContext(ctx, "BEGIN")
+// 	}
+// 	if err != nil {
+// 		return fmt.Errorf("failed to start transaction: %w", err)
+// 	}
+// 	defer func() {
+// 		if err := recover(); err != nil {
+// 			if _, rollbackErr := conn.ExecContext(ctx, "ROLLBACK"); rollbackErr != nil {
+// 				fmt.Printf("failed to rollback transaction in panic: %v", rollbackErr)
+// 			}
+// 			var e Error
+// 			if ok := errors.As(err.(error), &e); ok {
+// 				rErr = e
+// 			} else {
+// 				panic(err) // Re-panic to propagate the error
+// 			}
+// 		}
+// 	}()
+// 	tx := &sqlxTx{
+// 		ctx:        ctx,
+// 		conn:       conn,
+// 		driverName: driver,
+// 	}
+// 	err = fn(tx)
+// 	if err != nil {
+// 		if _, rollbackErr := conn.ExecContext(ctx, "ROLLBACK"); rollbackErr != nil {
+// 			return fmt.Errorf("failed to rollback transaction: %w", rollbackErr)
+// 		}
+// 		return err
+// 	}
+// 	_, err = conn.ExecContext(ctx, "COMMIT")
+// 	if err != nil {
+// 		return fmt.Errorf("failed to commit transaction: %w", err)
+// 	}
+// 	return nil
+// }
---
+++ tx_wrapper.go
@@ -0,0 +1,125 @@
+package sqlt
+
+import (
+	"github.com/jmoiron/sqlx"
+)
+
+type txWrapper struct {
+	tx *sqlx.Tx
+}
+
+func (tx *txWrapper) Exec(query string, args ...any) (Result, error) {
+	r, err := tx.tx.Exec(query, args...)
+	if err != nil {
+		return nil, err
+	}
+	return sqltResult{r}, nil
+}
+
+func (tx *txWrapper) MustExec(query string, args ...any) Result {
+	res, err := tx.Exec(query, args...)
+	if err != nil {
+		panic(Error{err})
+	}
+	return sqltResult{res}
+}
+
+func (tx *txWrapper) Query(query string, args ...any) (*sqlx.Rows, error) {
+	r, err := tx.tx.Queryx(query, args...)
+	if err != nil {
+		return nil, err
+	}
+	return r, nil
+}
+
+func (tx *txWrapper) MustQuery(query string, args ...any) *sqlx.Rows {
+	r, err := tx.Query(query, args...)
+	if err != nil {
+		panic(Error{err})
+	}
+	return r
+}
+
+func (tx *txWrapper) QueryRow(query string, args ...any) *sqlx.Row {
+	r := tx.tx.QueryRowx(query, args...)
+	if r == nil {
+		return nil
+	}
+	return r
+}
+
+func (tx *txWrapper) MustQueryRow(query string, args ...any) *sqlx.Row {
+	r := tx.QueryRow(query, args...)
+	if r == nil {
+		panic(Error{nil})
+	}
+	return r
+}
+
+func (tx *txWrapper) Get(dest any, query string, args ...any) error {
+	err := tx.tx.Get(dest, query, args...)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (tx *txWrapper) MustGet(dest any, query string, args ...any) {
+	err := tx.Get(dest, query, args...)
+	if err != nil {
+		panic(Error{err})
+	}
+}
+
+func (tx *txWrapper) Select(dest any, query string, args ...any) error {
+	err := tx.tx.Select(dest, query, args...)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (tx *txWrapper) MustSelect(dest any, query string, args ...any) {
+	err := tx.Select(dest, query, args...)
+	if err != nil {
+		panic(Error{err})
+	}
+}
+
+func (tx *txWrapper) Commit() error {
+	err := tx.tx.Commit()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (tx *txWrapper) Rollback() error {
+	err := tx.tx.Rollback()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (tx *txWrapper) DriverName() string {
+	return tx.tx.DriverName()
+}
+
+func (tx *txWrapper) Rebind(query string) string {
+	return tx.tx.Rebind(query)
+}
+
+func (tx *txWrapper) SelectIn(dest any, query string, args ...any) error {
+	p, q, err := sqlx.In(query, args...)
+	if err != nil {
+		return err
+	}
+	return tx.tx.Select(dest, p, q)
+}
+
+func (tx *txWrapper) MustSelectIn(dest any, query string, args ...any) {
+	err := tx.SelectIn(dest, query, args...)
+	if err != nil {
+		panic(Error{err})
+	}
+}
